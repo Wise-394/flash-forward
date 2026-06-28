edd65a8037595faa940e3e18b78dc9e5a61c6da3
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2026-06-28 16:46:29 +0800

    feat: implement username submission and navigation in registration screen

--- src/app/onboarding/register.tsx
+++ src/app/onboarding/register.tsx
@@ -2,11 +2,18 @@ import Screen from "@/components/Screen";
 import { AppInput } from "@/components/ui/appInput";
 import { AppText } from "@/components/ui/appText";
 import { WideButton } from "@/components/ui/wideButton";
+import { setUsername } from "@/services/storage/user/userService";
+import { router } from "expo-router";
 import { useState } from "react";
 import { View } from "react-native";
 
 export default function Register() {
   const [usernameInput, setUsernameInput] = useState("");
+
+  const handleSubmit = () => {
+    setUsername(usernameInput);
+    router.replace("/");
+  };
 
   return (
     <Screen>
@@ -20,7 +27,7 @@ export default function Register() {
         <AppText className="text-center text-lg">Enter Your Nickname</AppText>
       </View>
       <View className="mt-auto w-full">
-        <WideButton label="Confirm" onClick={() => {}} />
+        <WideButton label="Confirm" onClick={handleSubmit} />
       </View>
     </Screen>
   );
